adf65f32c5b1f28c35bea97cfa7bd02329e57059
drumi <andreyprodromov@gmail.com>
2023-11-22 15:51:26 +0200

    Added clone command

---
+++ src/main/java/com/andreyprodromov/commands/CloneCommand.java
@@ -0,0 +1,2 @@
+package com.andreyprodromov.commands;public class CloneCommand {
+}
---
+++ src/main/java/com/andreyprodromov/runtime/exceptions/EnvironmentAlreadyExistsException.java
@@ -0,0 +1,2 @@
+package com.andreyprodromov.runtime.exceptions;public class EnvironmentAlreadyExistsException {
+}
